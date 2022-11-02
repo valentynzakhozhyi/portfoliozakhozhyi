a18a64ad39bd7665df48792d2818d6a9a38c9067
valentynzakhozhyi <valentyn.zakhozhyi@outlook.com>
2022-11-02 13:09:54 +0200

    4 commit

---
+++ src/components/clickCount.tsx
@@ -0,0 +1,26 @@
+
+import React, {useEffect, useState } from 'react';
+import styles from "./clickCount.module.scss"
+
+type ClickCountProps = {
+
+}
+
+const ClickCount: React.FC<ClickCountProps> = () => {
+  const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        console.log('counter: ', counter);
+    }, [counter]);
+  return (
+    <div>
+    <p>You clicked {count} times</p>
+    <button className={styles.button} onClick={() => setCount(count + 1)}>
+      Click me
+    </button>
+    </div>
+  )
+}
+
+export default ClickCount
