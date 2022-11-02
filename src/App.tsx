51399b0037e150269980d1814d42664db98a66a5
valentynzakhozhyi <valentyn.zakhozhyi@outlook.com>
2022-11-02 11:47:08 +0200

    3 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // App.tsx
 import React from 'react'
 import MyFirstComponent from './components/myFirstComponent';
+import SomeExampleComponent from './components/example/someExampleComponent';
 
 const App: React.FC = () => {
   return (
     <div>
       <MyFirstComponent />
+      <SomeExampleComponent />
     </div>
   )
 }
