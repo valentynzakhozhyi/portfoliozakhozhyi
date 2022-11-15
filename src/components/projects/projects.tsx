bdd06d85980e99efa86b233c5adc19f0e8cc142f
valentynzakhozhyi <96794705+valentynzakhozhyi@users.noreply.github.com>
2022-11-15 12:05:11 +0200

    links

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import { IRepo } from '../types/gitHubData';
 import styles from "./projects.module.scss"
-import {Link} from "react-router-dom";
+
 
 type ProjectsProps = {
 
 }
 
 const Projects: React.FC<ProjectsProps> = () => {
+
+  const endpoint = "https://api.github.com/users/valentynzakhozhyi/repos";
+  const [githubData, setGithubData] = useState<IRepo[]>([]);
+
+  useEffect(() => {
+    fetch(endpoint)
+      .then(x => x.json())
+      .then(x => setGithubData(x))
+  }, [])
   return (
     <div className={styles.cont}>
-    <div >
-        <ul className={styles.links}>
-            <li><a href="https://github.com/valentynzakhozhyi/CalculatorPhyton">CalculatorPhyton</a></li>
-            <li><a href="https://github.com/valentynzakhozhyi/Ryhm-_3_Peli">Ryhm-_3_Peli</a></li>
-            <li><a href="https://github.com/valentynzakhozhyi/varia-project">varia-project</a> </li>
-      
-      </ul>
+      <div className={styles.list}>
+        {
+          githubData && githubData.map(x => (
+            <div key={x.id} className={styles.box}>
+              <h4 className={styles.title}>{x.name}</h4>
+              <p className={styles.text}>{x.description}</p>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
 }
-
 export default Projects
